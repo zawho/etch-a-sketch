ac470e59da18cd51b772bcde0b049776bc792bcd
zawho <zthooper2@gmail.com>
2022-09-12 11:53:13 +0800

    Add hover event loop

--- main.js
+++ main.js
@@ -4,6 +4,15 @@ let docFragment = document.createDocumentFragment();
 for (let i = 0; i < 256; i++) {
     const gridTile = document.createElement('div');
     gridTile.classList.add('grid-tile');
+    gridTile.id = 'tile-' + i;
     docFragment.appendChild(gridTile);
 }
 gridContainer.appendChild(docFragment);
+const allTiles = document.querySelectorAll('.grid-tile');
+
+//Hover event loop.
+for (let i = 0; i < allTiles.length; i++) {
+    allTiles[i].addEventListener('mouseover', function() {
+        allTiles[i].style.backgroundColor = 'black';
+    });
+}
